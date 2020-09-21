d62569b696f406a55a3c271bde847ccf190f4fc9
Lúcás Meier <cronokirby@gmail.com>
2020-09-21 18:10:21 +0200

    Add tailwind support

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="bg-red-300">Hello</h1>
+      <p className="font-mono bg-green-400">What's up?</p>
     </div>
   );
 }
