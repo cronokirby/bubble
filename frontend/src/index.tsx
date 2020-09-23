9359ac7a7f05a67303e23f9c75bed381b88adc56
Lúcás Meier <cronokirby@gmail.com>
2020-09-23 22:08:58 +0200

    Migrate to typescript

---
+++ frontend/src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "react-dom";
+
+function Application() {
+  return <h1>Application</h1>;
+}
+
+render(<Application />, document.getElementById("root"));
