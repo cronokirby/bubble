c7034792ed0991fad9387ea841681ff21aceddc4
Lúcás Meier <cronokirby@gmail.com>
2020-09-26 21:45:07 +0200

    Rename BubbleRef -> BubbleID

--- frontend/src/BubbleID.ts
+++ frontend/src/BubbleID.ts
@@ -4,15 +4,15 @@
  * Internally, this is represented as a 64 bit integer, hence the need for a BigInt.
  * You could use a string instead, but this representation should be more efficient
  */
-export type BubbleRef = bigint & { readonly __tag: unique symbol };
+export type BubbleID = bigint & { readonly __tag: unique symbol };
 
 /**
  * Create a BubbleRef from a string
  *
  * @param str the string
  */
-export function fromString(str: string): BubbleRef {
-  return BigInt(str) as BubbleRef;
+export function fromString(str: string): BubbleID {
+  return BigInt(str) as BubbleID;
 }
 
 /**
@@ -20,7 +20,7 @@ export function fromString(str: string): BubbleRef {
  *
  * @param ref the reference to a bubble
  */
-export function getDate(ref: BubbleRef): Date {
+export function getDate(ref: BubbleID): Date {
   const r = ref as bigint;
   return new Date(Number((r >> BigInt(4)) / BigInt(1000)));
 }
