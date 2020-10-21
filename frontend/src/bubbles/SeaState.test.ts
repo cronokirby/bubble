8d7447fa6d32132626781eb576cd41c10d121e6e
Lúcás Meier <cronokirby@gmail.com>
2020-10-21 23:45:11 +0200

    Create SeaProvider

--- frontend/src/bubbles/SeaState.test.ts
+++ frontend/src/bubbles/SeaState.test.ts
@@ -1,4 +1,4 @@
-import { Sea } from "./Sea";
+import { SeaState } from "./SeaState";
 import { NoRemoteSea } from "./RemoteSea";
 import * as BubbleID from "../BubbleID";
 
@@ -8,27 +8,27 @@ const id2 = BubbleID.fromString("0x2");
 const bubble2 = { inner: "2", children: [] };
 
 test("modify -> lookup should return the modification", async () => {
-  let sea = Sea.using(new NoRemoteSea());
+  let sea = SeaState.using(new NoRemoteSea());
   sea = await sea.modify(id1, bubble1);
   const res = await sea.lookup(id1);
   expect(res.bubble).toEqual(bubble1);
 });
 
 test("creating a sea with bubbles lets us look them up", async () => {
-  let sea = Sea.using(new NoRemoteSea(), [id1, bubble1], [id2, bubble2]);
+  let sea = SeaState.using(new NoRemoteSea(), [id1, bubble1], [id2, bubble2]);
   expect((await sea.lookup(id1)).bubble).toEqual(bubble1);
   expect((await sea.lookup(id2)).bubble).toEqual(bubble2);
 });
 
 test("linking works as expected", async () => {
-  let sea = Sea.using(new NoRemoteSea(), [id1, bubble1], [id2, bubble2]);
+  let sea = SeaState.using(new NoRemoteSea(), [id1, bubble1], [id2, bubble2]);
   sea = await sea.link(id1, id2);
   const expected = { ...bubble2, children: [id1] };
   expect((await sea.lookup(id2)).bubble).toEqual(expected);
 });
 
 test("unlinking works as expected", async () => {
-  let sea = Sea.using(new NoRemoteSea(), [id1, bubble1], [id2, bubble2]);
+  let sea = SeaState.using(new NoRemoteSea(), [id1, bubble1], [id2, bubble2]);
   sea = await sea.link(id1, id2);
   sea = await sea.unlink(id1, id2);
   expect((await sea.lookup(id2)).bubble).toEqual(bubble2);
