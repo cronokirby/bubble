12d4c0c72c1a9cd80946769ed1943fc549b25923
Lúcás Meier <cronokirby@gmail.com>
2020-11-02 01:10:20 +0100

    Serve dist file (in a hardcoded way)

--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
+
+const dir = "../frontend/dist"
+const addr = "127.0.0.1:3000"
 
 func main() {
 	fmt.Println("Hello World?")
+
+	server := http.FileServer(http.Dir(dir))
+	http.Handle("/", server)
+
+	log.Printf("Listening on %s\n", addr)
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
