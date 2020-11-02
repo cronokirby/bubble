0fc0f86d277c7e3e4befaf77fc59d34a1a74c971
Lúcás Meier <cronokirby@gmail.com>
2020-11-02 15:28:09 +0100

    Setup post route for modifying bubbles

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/go-chi/chi"
 )
+
+type modifyBubbleT struct {
+	Bubble string
+}
+
+func modifyBubble(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	decoder := json.NewDecoder(r.Body)
+	var data modifyBubbleT
+	if err := decoder.Decode(&data); err != nil {
+		http.Error(w, http.StatusText(400), 400)
+		return
+	}
+	fmt.Println(id, data)
+	w.WriteHeader(200)
+	w.Write([]byte("{}"))
+}
 
 func main() {
 	dir := flag.String("dir", "../frontend/dist", "the directory for static assets")
@@ -15,8 +34,9 @@ func main() {
 
 	r := chi.NewRouter()
 
-	server := http.FileServer(http.Dir(*dir))
-	r.Get("/*", server.ServeHTTP)
+	fileServer := http.FileServer(http.Dir(*dir))
+	r.Post("/api/bubble/{id}", modifyBubble)
+	r.Handle("/*", fileServer)
 
 	log.Printf("Listening on %s\n", *addr)
 	err := http.ListenAndServe(*addr, r)
